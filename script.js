09aa498d187e9b425948fcad02d88018c7752e89
schoolop08 <soserohini11.9a.12.omprakashsahoo@gmail.com>
2024-10-07 19:26:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 
 // List of prohibited names or words
-var prohibitedNames = ["baap", "ka", "ki"];
+var prohibitedNames = [
+  "papa", "baap", "maa", "madarchod", "bahenchod", "gandu", "chutiya",
+  "bhosdike", "laude", "loda", "lodu", "chut", "lund", "haraami", "kutta",
+  "kamina", "saala", "chakka", "hijra", "randi", "randwa", "kutti", "suar",
+  "gaand", "bitch", "fuck", "motherfucker", "asshole", "bastard", "slut",
+  "dick", "pussy", "cunt", "whore", "nigger", "shit", "dumbass", "retard",
+  "twat", "prick", "wanker", "piss", "jerk", "cock", "fucker", "ass", "arse",
+  "shag", "dildo", "bollocks", "fag", "scum", "tosser", "bollocks", "bhen", 
+  "mc", "bc", "chod", "chutiya", "pataka", "jhant", "rakhail", "kameena", 
+  "tatti", "sala", "chuti", "kutti", "haraamzada", "bancho", "haramkhor", 
+  "ullu", "bevda", "nashedi", "chhakka", "faadu", "bakchod", "jhaant", "chutya",
+  "loda", "raand", "gand", "bhenchod", "madar", "gaand", "suar", "kalank", "maa ki", 
+  "bhen ki", "gaandu", "kamina", "ullu ke pathe", "saale", "kutiya", "choot", "kutta"
+];
+
 
 // Check if a username already exists in local storage
 var storedUser = localStorage.getItem("username");
